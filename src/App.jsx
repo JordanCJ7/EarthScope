9d390319bc0f076bbc1e56aba7432ad13e364756
JordanCJ7 <janithasuranjana2001@gmail.com>
2025-05-06 14:14:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './pages/Home';
+import HomePage from './pages/HomePage';
 import CountryDetails from './pages/CountryDetails';
 
 const App = () => {
-  return (
-    <Router>
-      <div>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/country/:name" component={CountryDetails} />
-        </Switch>
-      </div>
-    </Router>
-  );
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then((response) => response.json())
+            .then((data) => setCountries(data));
+    }, []);
+
+    return (
+        <Router>
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/" exact>
+                        <HomePage countries={countries} />
+                    </Route>
+                    <Route path="/country/:name" component={CountryDetails} />
+                </Switch>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
