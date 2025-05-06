b4ed6455bfbe20c9ad59f077311fbeb20a620ed0
JordanCJ7 <janithasuranjana2001@gmail.com>
2025-05-06 14:14:14 +0530

    Update CountryCard.jsx

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 const CountryCard = ({ country }) => {
     return (
         <div className="country-card">
-            <img src={country.flags.png} alt={`Flag of ${country.name}`} />
-            <h2>{country.name}</h2>
+            <img src={country.flags?.png} alt={`Flag of ${country.name?.common}`} />
+            <h2>{country.name?.common}</h2>
             <p><strong>Population:</strong> {country.population}</p>
             <p><strong>Region:</strong> {country.region}</p>
-            <p><strong>Capital:</strong> {country.capital}</p>
-            <p><strong>Languages:</strong> {Object.values(country.languages).join(', ')}</p>
+            <p><strong>Capital:</strong> {country.capital?.[0] || 'N/A'}</p>
+            <p>
+                <strong>Languages:</strong>{' '}
+                {country.languages
+                    ? Object.values(country.languages).join(', ')
+                    : 'N/A'}
+            </p>
         </div>
     );
 };
