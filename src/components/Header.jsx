8231f3ec56a3c0c1cad9226b3c48d3fb228609cb
JordanCJ7 <janithasuranjana2001@gmail.com>
2025-05-08 02:01:34 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
+import { FaGlobeAmericas } from 'react-icons/fa';
 
 const Header = () => {
     return (
-        <header>
-            <h1>EarthScope</h1>
-            <input type="text" placeholder="Search for a country..." />
-            <select>
-                <option value="">Filter by Region</option>
-                <option value="Africa">Africa</option>
-                <option value="Americas">Americas</option>
-                <option value="Asia">Asia</option>
-                <option value="Europe">Europe</option>
-                <option value="Oceania">Oceania</option>
-            </select>
+        <header className="app-header">
+            <div className="header-content">
+                <FaGlobeAmericas className="header-icon" />
+                <span className="header-title">EarthScope</span>
+            </div>
         </header>
     );
 };
