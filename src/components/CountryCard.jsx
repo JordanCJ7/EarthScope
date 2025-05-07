002078ffffaf4c0edda9f4efe650d9461c3af440
JordanCJ7 <janithasuranjana2001@gmail.com>
2025-05-08 02:01:31 +0530

    Update CountryCard.jsx

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
+import { FaUsers, FaGlobe, FaMapMarkerAlt } from 'react-icons/fa';
 
 const CountryCard = ({ country }) => {
     return (
         <div className="country-card">
-            <img src={country.flags?.png} alt={`Flag of ${country.name?.common}`} />
-            <h2>{country.name?.common}</h2>
-            <p><strong>Population:</strong> {country.population}</p>
-            <p><strong>Region:</strong> {country.region}</p>
-            <p><strong>Capital:</strong> {country.capital?.[0] || 'N/A'}</p>
-            <p>
-                <strong>Languages:</strong>{' '}
-                {country.languages
-                    ? Object.values(country.languages).join(', ')
-                    : 'N/A'}
-            </p>
+            <img className="country-flag" src={country.flags?.png} alt={`Flag of ${country.name?.common}`} />
+            <h2 className="country-name">{country.name?.common}</h2>
+            <div className="country-details">
+                <p><FaUsers /> Population: {country.population.toLocaleString()}</p>
+                <p><FaGlobe /> Region: {country.region}</p>
+                <p><FaMapMarkerAlt /> Capital: {country.capital?.[0] || 'N/A'}</p>
+            </div>
+            <button className="details-btn">View Details</button>
         </div>
     );
 };
