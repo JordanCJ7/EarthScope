b5db340085843021939d43505a862d4ab4480ab5
JordanCJ7 <janithasuranjana2001@gmail.com>
2025-05-08 16:56:15 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { FaGlobeAmericas } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className="app-header">
             <div className="header-content">
-                <FaGlobeAmericas className="header-icon" />
-                <span className="header-title">EarthScope</span>
+                <Link to="/" style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'inherit' }}>
+                    <FaGlobeAmericas className="header-icon" />
+                    <span className="header-title">EarthScope</span>
+                </Link>
             </div>
         </header>
     );
