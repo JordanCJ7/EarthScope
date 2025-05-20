8920d5c9d2efb9b1dd2766a02faea8f619aaf816
JordanCJ7 <janithasuranjana2001@gmail.com>
2025-05-20 10:11:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import CountryDetails from './pages/CountryDetails';
+import LoginRegister from './components/LoginRegister';
+import { AuthContext } from './context/AuthContext';
 
 const App = () => {
     const [countries, setCountries] = useState([]);
+    const { user } = useContext(AuthContext);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
@@ -16,12 +19,15 @@ const App = () => {
     return (
         <Router>
             <div>
-                <Header />
+                {user && <Header />}
                 <Switch>
-                    <Route path="/" exact>
-                        <HomePage countries={countries} />
+                    <Route path="/login" exact>
+                        {user ? <Redirect to="/" /> : <LoginRegister />}
                     </Route>
-                    <Route path="/country/:code" component={CountryDetails} />
+                    <Route path="/" exact>
+                        {user ? <HomePage countries={countries} /> : <Redirect to="/login" />}
+                    </Route>
+                    <Route path="/country/:code" render={(props) => user ? <CountryDetails {...props} /> : <Redirect to="/login" />} />
                 </Switch>
             </div>
         </Router>
