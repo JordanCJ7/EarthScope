aca7e43487730d0c29e689abb5acf9f3d5347d21
JordanCJ7 <janithasuranjana2001@gmail.com>
2025-05-20 10:12:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; // Use the new API
 import App from './App';
 import './styles/App.css';
+import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
